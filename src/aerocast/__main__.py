a7f9bc83b81a69c2ebf3e4df4ddc6082a2f91bb4
Charles Fournier <charles.fournier@sodevnet.com>
2024-05-11 15:51:52 +0200

    maj weather, main

--- src/aerocast/__main__.py
+++ src/aerocast/__main__.py
@@ -4,6 +4,7 @@ from aerocast.airport import AirportManager
 
 
 def main():
+
     print("Bienvenue dans AeroCast, votre gestionnaire de météo d'aéroport!")
     
     # Exemple d'utilisation des gestionnaires
@@ -12,22 +13,11 @@ def main():
 
     weather = WeatherManager(airport_code, lang)
 
-    print(weather.get_temperature())
+    weather.get_summarize()
+    #weather.get_wind_speed()
+    #weather.get_temperature()
 
-
-    # airport_manager = AirportManager(airport_code)
-    
-    # Récupérer les informations
-    #print(weather_manager.summarize())
-   
-
-    #print(weather_manager.get_wind_speed())
-    # airport_info = airport_manager.get_airport_info()
-    
-    # print(airport_manager.summarize())
-    
-
- 
+    #airport_manager = AirportManager(airport_code)
 
 if __name__ == "__main__":
     main()
--- src/aerocast/weather.py
+++ src/aerocast/weather.py
@@ -1,15 +1,18 @@
 # aerocast/weather.py
 from .data_manager import DataManager
 from .tts_manager import TextToSpeechManager
-
+import threading
+import os
 
 class WeatherManager:
+
+    AUDIO_OUTPUT_PATH = "info.mp3"
+
     def __init__(self, airport_code, lang):
         self.airport_code = airport_code
         self.lang = lang
     
     def fetch_weather_data(self):
-        # Mise en cache des données pour réduire les appels API inutiles
         weather_data = DataManager.get_weather_data(self.airport_code)
         if not weather_data:
             raise ValueError("No weather data available")
@@ -17,23 +20,73 @@ class WeatherManager:
          
     def get_temperature(self):
         weather_data = self.fetch_weather_data()
-        result = f"{weather_data.get('temp', 'N/A')}°C"
+        result = f"{weather_data.get('temp', 'N/A')}"
+
         if self.lang:
-            print(self.lang)
             result = f"La température est de {weather_data.get('temp', 'N/A')}°C"
-            print(result)
             tts_weather = TextToSpeechManager(result, self.lang)
-            tts_weather.save_to_file("weather_info.mp3")
-            tts_weather.play_file("weather_info.mp3")
+            thread_save = threading.Thread(target=self.save_audio, args=(tts_weather,))
+            thread_play = threading.Thread(target=self.play_audio, args=(tts_weather,))
+            thread_save.start()
+            thread_save.join()  # Attendre que la sauvegarde soit complète avant de lire
+            thread_play.start()
+
         return result
 
     def get_wind_speed(self):
         weather_data = self.fetch_weather_data()
-        return f"Wind Speed: {weather_data.get('wind_speed', 'N/A')} km/h"
+        result = f"{weather_data.get('wind_speed', 'N/A')}"
+    
+        if self.lang:
+            result = f"La vitesse du vent est de {weather_data.get('temp', 'N/A')} km/h"
+            tts_weather = TextToSpeechManager(result, self.lang)
+            thread_save = threading.Thread(target=self.save_audio, args=(tts_weather,))
+            thread_play = threading.Thread(target=self.play_audio, args=(tts_weather,))
+            thread_save.start()
+            thread_save.join()  # Attendre que la sauvegarde soit complète avant de lire
+            thread_play.start()
 
-    def summarize(self):
+        return result
+
+
+    def get_summarize(self):
+        # Récupérer les données météorologiques
         weather_data = self.fetch_weather_data()
-        temperature = weather_data.get('temp', 'N/A')
-        wind_speed = weather_data.get('wind_speed', 'N/A')
-        return f"Temp: {temperature}°C, Wind: {wind_speed} km/h"
 
+        print(weather_data)
+
+        # Préparer les parties du message avec les données récupérées ou des valeurs par défaut
+        wind_direction = weather_data.get('wdir', 'N/A')
+        wind_speed = weather_data.get('wspd', 'N/A')
+        cloud_cover = weather_data.get('clouds', 'N/A')
+        cloud_description = weather_data.get('clouds_text', 'N/A')
+        visibility = weather_data.get('visib', 'N/A')
+        temperature = weather_data.get('temp', 'N/A')
+        dew_point = weather_data.get('dewp', 'N/A')
+        pressure = weather_data.get('altim', 'N/A')
+
+        # Construction du résumé météorologique
+        meteo_text = (
+            f"Le METAR indique un vent de {wind_direction} degrés, avec une vitesse de {wind_speed} nœuds. "
+            f"D'après les codes de temps, nous avons un {cloud_cover}, ce qui signifie des conditions {cloud_description}. "
+            f"La distance de visibilité est de {visibility} kilomètres. "
+            f"La température est de {temperature} degrés Celsius, et le point de rosée est de {dew_point} degrés Celsius. "
+            f"La pression atmosphérique QNH est située à {pressure} hPa."
+        )
+
+        return meteo_text
+
+
+
+    def save_audio(self, tts_weather):
+        tts_weather.save_to_file(self.AUDIO_OUTPUT_PATH)
+        if os.path.exists(self.AUDIO_OUTPUT_PATH) and os.path.getsize(self.AUDIO_OUTPUT_PATH) > 0:
+            print("File saved successfully and contains data")
+        else:
+            print("Failed to save file or file is empty")
+
+    def play_audio(self, tts_weather):
+        if os.path.exists(self.AUDIO_OUTPUT_PATH) and os.path.getsize(self.AUDIO_OUTPUT_PATH) > 0:
+            tts_weather.play_file(self.AUDIO_OUTPUT_PATH)
+        else:
+            print("Cannot play audio because the file does not exist or is empty")
