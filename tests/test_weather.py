6ad94749244b494d17c004944bfab9a7d21fc1f3
DarkRedman <contact@darkredman.fr>
2024-05-13 23:47:41 +0200

    Mise à jour de test_weather.py pour le type attendu

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -3,9 +3,17 @@ import unittest
 from aerocast.weather import WeatherManager
 
 class TestWeatherManager(unittest.TestCase):
+    def test_temperature(self):
+        wm = WeatherManager('KJFK', lang=None)
+        self.assertIsInstance(wm.get_temperature(), str)
+    
     def test_summarize(self):
         wm = WeatherManager('KJFK', lang=None)
         self.assertIsInstance(wm.get_summarize(), str)
+
+    def test_wind_speed(self):
+        wm = WeatherManager('KJFK', lang=None)
+        self.assertIsInstance(wm.get_wind_speed(), str)
 
 if __name__ == '__main__':
     unittest.main()
